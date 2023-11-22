6756a4732a8d22393655ed01b63b85c89107fc8a
gwinbleadd <g.gero2010@yandex.ru>
2023-11-22 21:01:53 +0300

    Ferth

---
+++ Example002/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Напишите ваше имя");//ввыводит просто тект на экран//
+string username = Console.ReadLine();//Console.ReadLine - Чтение текста, введенного в строке пользователем; String - строчный метод, что дает понять что метод идет в строку буквами.//
+Console.Write("Hello, ");//Console.Write ввыводит просто тект на экран без переноса текста//
+Console.Write(username);
