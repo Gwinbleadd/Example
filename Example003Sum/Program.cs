2de016393dabc87ba58d6f12cbb8c6a1cf944275
gwinbleadd <g.gero2010@yandex.ru>
2023-11-22 21:47:33 +0300

    Example 002 Random

--- Example003Sum/Program.cs
+++ Example003Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 10;//int метод целых чисел
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);//1 2 3 4 5 6 7 8 9   9 - max
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;//result -  промежуточная переменная
 Console.WriteLine(result);
