dbf9100f32447c4b7c7de0ee455615a0a106ffca
gwinbleadd <g.gero2010@yandex.ru>
2023-11-22 22:22:47 +0300

    Example 006 Задача на максимальное значение

--- Example005 Петя/Program.cs
+++ Example005 Петя/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine(" ВВедите имя пользователя: ");
 string username = Console.ReadLine();
-if (username.ToLower == "Петя")
+if (username.ToLower == "Петя")//ToLower -  позволяет все символы в строке перевести в нижний регистр
 {
 Console.WriteLine("Пошли пить пиво,мой дорогой");
 }
