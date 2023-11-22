fb915dc1d0aa24bc84dc42525e46f1bfb5cdc3cc
gwinbleadd <g.gero2010@yandex.ru>
2023-11-22 22:09:42 +0300

    Example 005 Приветсвие user

---
+++ Example005 Петя/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(" ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower == "Петя")
+{
+Console.WriteLine("Пошли пить пиво,мой дорогой");
+}
+else 
+{
+Console.Write("Здарова , ");
+Console.Write(username);
+}
